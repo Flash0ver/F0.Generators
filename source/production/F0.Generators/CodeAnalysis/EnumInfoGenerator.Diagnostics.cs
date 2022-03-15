91395b5c79874c2ef2e86df9d097579f44672906
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2022-03-15 20:11:43 +0100

    build: fix RS2003

--- source/production/F0.Generators/CodeAnalysis/EnumInfoGenerator.Diagnostics.cs
+++ source/production/F0.Generators/CodeAnalysis/EnumInfoGenerator.Diagnostics.cs
@@ -2,9 +2,10 @@ using F0.Diagnostics;
 
 namespace F0.CodeAnalysis;
 
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0090:Use 'new(...)'", Justification = "https://github.com/dotnet/roslyn-analyzers/issues/5828")]
 internal partial class EnumInfoGenerator
 {
-	private static readonly DiagnosticDescriptor UnspecializedPlaceholder = new(
+	private static readonly DiagnosticDescriptor UnspecializedPlaceholder = new DiagnosticDescriptor(
 		DiagnosticIds.F0GEN0301,
 		$"Do not use the unspecialized placeholder method of '{TypeName}.{MethodName}({nameof(Enum)})'",
 		$"Do not use the unspecialized placeholder method of '{TypeName}.{MethodName}({nameof(Enum)})' with non-enum argument '{{0}}'",
@@ -15,7 +16,7 @@ internal partial class EnumInfoGenerator
 		DiagnosticHelpLinkUris.F0GEN0301
 	);
 
-	private static readonly DiagnosticDescriptor AmbiguousConfiguration = new(
+	private static readonly DiagnosticDescriptor AmbiguousConfiguration = new DiagnosticDescriptor(
 		DiagnosticIds.F0GEN0302,
 		$"Ambiguous configuration of '{nameof(EnumInfoGenerator)}'",
 		$"Ambiguous configuration of '{nameof(EnumInfoGenerator)}': Global AnalyzerConfig: 'f0gen_enum_throw = {{0}}' -and- MSBuild Property: '<F0Gen_EnumInfo_ThrowIfConstantNotFound>{{1}}</F0Gen_EnumInfo_ThrowIfConstantNotFound>'",
--- source/production/F0.Generators/Shared/SourceGenerationExceptionGenerator.Diagnostics.cs
+++ source/production/F0.Generators/Shared/SourceGenerationExceptionGenerator.Diagnostics.cs
@@ -2,9 +2,10 @@ using F0.Diagnostics;
 
 namespace F0.Shared;
 
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0090:Use 'new(...)'", Justification = "https://github.com/dotnet/roslyn-analyzers/issues/5828")]
 internal partial class SourceGenerationExceptionGenerator
 {
-	private static readonly DiagnosticDescriptor AvoidUsage = new(
+	private static readonly DiagnosticDescriptor AvoidUsage = new DiagnosticDescriptor(
 		DiagnosticIds.F0GEN0101,
 		$"Avoid using '{TypeName}' directly",
 		$"Avoid using '{TypeName}' directly: '{{0}}'",
